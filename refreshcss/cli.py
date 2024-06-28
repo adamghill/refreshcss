8fa8777c592d3788bc8fecfd40c18d2ee592fbad
Tobias Bölz <tobias@boelz.eu>
2024-06-28 20:34:38 +0200

    Use click.File and click.Path

--- refreshcss/cli.py
+++ refreshcss/cli.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import click
 
 from refreshcss.html.site import FilesSite
@@ -7,16 +5,15 @@ from refreshcss.main import RefreshCSS
 
 
 @click.command()
-@click.option("-o", "--output", help="Write to file instead of stdout.")
+@click.option("-o", "--output", type=click.File("w"), default="-", help="Write to file instead of stdout.")
 @click.option("-R", "-r", "--recursive", is_flag=True, help="Recursively search subdirectories listed.")
-@click.argument("css",)
-@click.argument("html", nargs=-1, required=True)
+@click.argument("css", type=click.File())
+@click.argument("html", type=click.Path(exists=True), nargs=-1, required=True)
 @click.version_option()
 def cli(output, recursive, css, html):
     """Remove classes, ids, and element selectors not used in HTML from CSS."""
-    css = Path(css).read_text()
+    css_text = css.read()
     site = FilesSite(html, recursive)
     site.parse()
     refresh_css = RefreshCSS(site)
-    with click.open_file(output or "-", "w") as f:
-        f.write(refresh_css.clean(css))
+    output.write(refresh_css.clean(css_text))
