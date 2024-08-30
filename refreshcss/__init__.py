4ad071e40369602287711aa2757db5d138b667c8
Tobi DEGNON <tobidegnon@proton.me>
2024-08-30 20:05:10 +0100

    import refresh cli only if click is installed

--- refreshcss/__init__.py
+++ refreshcss/__init__.py
@@ -1,9 +1,12 @@
-from refreshcss.cli import cli
 from refreshcss.html.site import DjangoSite, PathSite
 from refreshcss.main import RefreshCSS
 
 __all__ = ["RefreshCSS", "DjangoSite", "PathSite"]
 
-
-if __name__ == "__main__":
-    cli()
+try:
+    from refreshcss.cli import cli
+except ImportError:
+    pass
+else:
+    if __name__ == "__main__":
+        cli()
